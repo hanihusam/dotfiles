c5e7e7010a2a24a30c368ae9482142c9f2803ba8
Zain Fathoni <me@zainf.dev>
2021-04-14 05:56:38 +0800

    Install Hyper featured plugins

--- hyper.js
+++ hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hypercwd", "hyper-search", "hyper-pane", "hyperpower"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
