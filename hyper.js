3157c5f15142135d95e55a94322646f2562a4c40
Zain Fathoni <me@zainf.dev>
2021-04-14 05:58:32 +0800

    Install hyper-night-owl theme

--- hyper.js
+++ hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hypercwd", "hyper-search", "hyper-pane", "hyperpower"],
+  plugins: ["hypercwd", "hyper-search", "hyper-pane", "hyperpower", "hyper-night-owl"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
