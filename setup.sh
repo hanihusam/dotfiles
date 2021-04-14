da2770a9e951e35904ed48841ebd88957aa19df2
Zain Fathoni <me@zainf.dev>
2021-04-15 05:53:36 +0800

    Prettify setup.sh prompts

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 echo "Setting up your Mac..."
 
+echo ""
+echo "=== Installing oh-my-zsh ==="
+echo ""
+
 # Check for Oh My Zsh and install if we don't have it
 if [[ ! -d "$HOME/.oh-my-zsh" ]] ; then
   # https://github.com/ohmyzsh/ohmyzsh#unattended-install
@@ -20,22 +24,39 @@ if [[ ! -d "$HOME/.oh-my-zsh" ]] ; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
+echo ""
+echo "=== Installing Homebrew ==="
+echo ""
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# Add Homebrew & oh-my-zsh PATH by installing zshrc through dotfiles
+echo ""
+echo "=== Installing dotfiles through dotbot ==="
+echo ""
+
+
+# Add Homebrew & oh-my-zsh PATH by installing .zshrc file through dotbot
 ./install
 
 # Update Homebrew recipes
-echo "Updating Homebrew recipes"
+echo ""
+echo "=== Updating Homebrew recipes ==="
+echo ""
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-echo "Installing Homebrew bundle defined in Brewfile"
+echo ""
+echo "=== Installing Homebrew bundle defined in Brewfile ==="
+echo ""
 brew tap homebrew/bundle
 brew bundle
+
+echo ""
+echo "=== Installing Volta ==="
+echo ""
 
 # Install Volta separately
 if test ! $(which volta); then
@@ -45,6 +66,10 @@ if test ! $(which volta); then
   volta install node
   volta install yarn
 fi
+
+echo ""
+echo "=== Initializing SSH ==="
+echo ""
 
 echo "Checking whether SSH key for me@zainf.dev is already generated or not"
 if ! grep -q me@zainf.dev ~/.ssh/id_ed25519.pub; then 
@@ -60,6 +85,10 @@ else
 fi
 echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
 
+echo ""
+echo "=== Initializing GPG ==="
+echo ""
+
 echo "Checking for existing GPG keys"
 if [[ $(gpg --list-secret-keys --keyid-format LONG) ]]; then
   echo "GPG keys found!"
@@ -72,11 +101,20 @@ echo "Then upload the public key to the remote repository"
 echo "https://docs.github.com/en/github/authenticating-to-github/generating-a-new-gpg-key"
 echo "And tell Git about your GPG key"
 echo "https://docs.github.com/en/github/authenticating-to-github/telling-git-about-your-signing-key"
+
 gpg --list-secret-keys --keyid-format LONG
+
+echo ""
+echo "=== Setting up macOS preferences ==="
+echo ""
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
 ./macos.sh
+
+echo ""
+echo "=== Performing final benchmark ==="
+echo ""
 
 # Call benchmark.zsh at the end of setup to assess the performance impact of the recent configuration
 ./bin/benchmark.zsh
