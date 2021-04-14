58a8a12dabe4d454f76f6bf85bd71aa4dd06638f
Zain Fathoni <me@zainf.dev>
2021-04-14 09:34:36 +0800

    Format hyper.js file

--- hyper.js
+++ hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hypercwd", "hyper-search", "hyper-pane", "hyperpower", "hyper-night-owl"],
+  plugins: ['hypercwd', 'hyper-search', 'hyper-pane', 'hyperpower', 'hyper-night-owl'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
@@ -157,4 +157,4 @@ module.exports = {
     // Example
     // 'window:devtools': 'cmd+alt+o',
   },
-};
+}
