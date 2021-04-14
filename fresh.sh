c198ff43241952e9442c09436f4644d57b37b1be
Zain Fathoni <me@zainf.dev>
2021-04-14 15:09:58 +0800

    Ignore Brewfile.lock.json and secrets.zsh

--- fresh.sh
+++ fresh.sh
@@ -65,6 +65,11 @@ echo "And tell Git about your GPG key"
 echo "https://docs.github.com/en/github/authenticating-to-github/telling-git-about-your-signing-key"
 gpg --list-secret-keys --keyid-format LONG
 
+# Export secret environment variables
+if [ -f "./secrets.zsh" ]; then
+  source ./secrets.zsh
+fi
+
 # Set macOS preferences
 # We will run this last because this will reload the shell
 source ./macos.sh
