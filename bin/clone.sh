68ee4fea92f164e945809ac15f9b111fed342f3e
Zain Fathoni <me@zainf.dev>
2021-04-15 08:00:44 +0800

    Complete URL shortener repositories setup

--- bin/clone.sh
+++ bin/clone.sh
@@ -5,9 +5,36 @@ echo "Cloning repositories..."
 GITHUB=$HOME/Code/GitHub
 GITLAB=$HOME/Code/GitLab
 
-# URL Shorteners
-if [[ ! -d "$GITLAB/kawalcovid19/kcov.id" ]] ; then
-  git clone git@gitlab.com:kawalcovid19/website/kcov.id.git $GITLAB/kawalcovid19/kcov.id
+echo ""
+echo "=== URL shortener repositories ==="
+echo ""
+
+if [[ ! -d "$GITHUB/zainfathoni/shortener" ]] ; then
+  git clone git@github.com:zainfathoni/shortener.git $GITHUB/zainfathoni/shortener
+  cd $GITHUB/zainfathoni/shortener
+  yarn
 fi
 
+if [[ ! -d "$GITHUB/zainfathoni/rbagi.id" ]] ; then
+  git clone git@github.com:zainfathoni/rbagi.id.git $GITHUB/zainfathoni/rbagi.id
+  cd $GITHUB/zainfathoni/rbagi.id
+  yarn
+fi
 
+if [[ ! -d "$GITHUB/zainfathoni/s.imas.sg" ]] ; then
+  git clone git@github.com:zainfathoni/s.imas.sg.git $GITHUB/zainfathoni/s.imas.sg
+  cd $GITHUB/zainfathoni/s.imas.sg
+  yarn
+fi
+
+if [[ ! -d "$GITLAB/kawalcovid19/website/kcov.id" ]] ; then
+  git clone git@gitlab.com:kawalcovid19/website/kcov.id.git $GITLAB/kawalcovid19/website/kcov.id
+  cd $GITLAB/kawalcovid19/website/kcov.id
+  yarn
+fi
+
+if [[ ! -d "$GITHUB/frontend-id/s.feid.dev" ]] ; then
+  git clone git@github.com:frontend-id/s.feid.dev.git $GITHUB/frontend-id/s.feid.dev
+  cd $GITHUB/frontend-id/s.feid.dev
+  yarn
+fi
--- zshrc
+++ zshrc
@@ -95,7 +95,11 @@ fi
 
 # URL Shorteners
 # https://github.com/kentcdodds/netlify-shortener#bash
-kcovid() { node $HOME/Code/GitLab/kawalcovid19/kcov.id/node_modules/.bin/netlify-shortener "$1" "$2"; }
+zainf() { node $HOME/Code/GitHub/zainfathoni/shortener/node_modules/.bin/netlify-shortener "$1" "$2"; }
+rbagi() { node $HOME/Code/GitHub/zainfathoni/rbagi.id/node_modules/.bin/netlify-shortener "$1" "$2"; }
+imas() { node $HOME/Code/GitHub/zainfathoni/s.imas.sg/node_modules/.bin/netlify-shortener "$1" "$2"; }
+kcovid() { node $HOME/Code/GitLab/kawalcovid19/website/kcov.id/node_modules/.bin/netlify-shortener "$1" "$2"; }
+feid() { node $HOME/Code/GitHub/frontend-id/s.feid.dev/node_modules/.bin/netlify-shortener "$1" "$2"; }
 
 # export MANPATH="/usr/local/man:$MANPATH"
 
