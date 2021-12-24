2497ddaae71d9037801128e2b139f922efc25260
Hani Husamuddin <hani.husam@gmail.com>
2021-12-24 19:16:53 +0700

    Update .personal.zsh

--- .personal.zsh
+++ .personal.zsh
@@ -47,12 +47,3 @@ fi
 
 # URL Shorteners
 # https://github.com/kentcdodds/netlify-shortener#bash
-zainf() { node $HOME/Code/GitHub/zainfathoni/shortener/node_modules/.bin/netlify-shortener "$1" "$2"; }
-rbagi() { node $HOME/Code/GitHub/zainfathoni/rbagi.id/node_modules/.bin/netlify-shortener "$1" "$2"; }
-imas() { node $HOME/Code/GitHub/zainfathoni/s.imas.sg/node_modules/.bin/netlify-shortener "$1" "$2"; }
-kcovid() { node $HOME/Code/GitLab/kawalcovid19/website/kcov.id/node_modules/.bin/netlify-shortener "$1" "$2"; }
-feid() { node $HOME/Code/GitHub/frontend-id/s.feid.dev/node_modules/.bin/netlify-shortener "$1" "$2"; }
-
-# Ninja Van Kubernetes context switching
-alias kube-prod='source ~/scripts/bin/kube/production.zsh'
-alias kube-dev='source ~/scripts/bin/kube/development.zsh'
